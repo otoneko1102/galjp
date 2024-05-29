34b6a5de4f2de68f94762af255fc8781e0caa435
otoneko1102 <rin_pineapple@au.com>
2024-05-29 13:01:31 +0900

    Update.

--- src/generate.js
+++ src/generate.js
@@ -1,8 +1,18 @@
+/*
+ * Generation - galmoji
+ * @param input string (required)
+ * @returns - Generated string.
+ */
+
 function generate(input) {
   if (!input || typeof input !== 'string') throw new Error('input must be string.');
 
-  const w = require('../lib/words.json');
-  const c = require('../lib/chars.json');
+  const w = require('../lib/word.json');
+  const alphabet = require('../lib/chars/alphabet.json');
+  const hira = require('../lib/chars/hiragana.json');
+  const kata = require('../lib/chars/katakana.json');
+  const kanji = require('../lib/chars/kanji.json');
+  const other = require('../lib/chars/other.json');
   let text = input;
   for (const word of Object.keys(w)) {
     const regex = new RegExp(word, 'g');
@@ -10,11 +20,13 @@ function generate(input) {
   }
   const chars = [];
   for (const char of text.split('')) {
-    if (c[char]) {
-      chars.push(c[char]);
-    } else {
-      chars.push(char);
-    }
+    let c = char;
+    if (alphabet[c.toUpperCase()]) c = alphabet[c.toUpperCase()];
+    if (hira[c]) c = hira[c];
+    if (kata[c]) c = kata[c];
+    if (kanji[c]) c = kanji[c];
+    if (other[c]) c = other[c];
+    chars.push(c);
   }
   const result = chars.join('');
 
